3199293bdb208aac3bd4c0c157124ffdda3601cd
garry <garry4978@gmail.com>
2023-02-21 04:27:46 +0300

    Урок 1. Spring. Введение

--- src/main/java/org/skypro/Main.java
+++ src/main/java/org/skypro/Main.java
@@ -2,15 +2,22 @@ package org.skypro;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
+//
+//1. Создайте maven-проект с архетипом maven-archetype-quickstart, добавьте в файл pom.xml зависимость spring-context.
+//2. Создайте папку resources, а в ней файл applicationContext и добавьте в него скрипт.
+//3. Создайте класс Car с методом go, который выводит в консоль “Автомобиль едет”.
+//4. Реализуйте Инверсию управления для этого класса и вызовите метод бина в методе main.
+//5. Добавьте класс Person с методом, в котором будет выводиться строка “Владелец сел в автомобиль” и вызываться метод автомобиля.
+//6. Реализуйте внедрение зависимостей в двух новых конфиг-файлах и вызовите метод объекта Person в новых классах.
+//7. Назначьте в новых конфиг-файлах бинам класса Car scope singleton и prototype. Создайте по 2 объекта в новых классах и сравните их.
+//8. Создайте в классе Car init и destroy методы, укажите эти методы в конфиг-файлах со скоупами. Реализуйте вызов этих методов (если это возможно).
+//
+
 public class Main
 {
     public static void main( String[] args )
     {
-
+        // задачи 1,2,3,4,5
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
@@ -26,7 +33,7 @@ public class Main
 
         context.close(); System.out.println("-------------------------------------------");
 
-
+        // задачи 6,8
         ClassPathXmlApplicationContext contextProto =
                 new ClassPathXmlApplicationContext("applicationContextProto.xml");
 
@@ -44,7 +51,7 @@ public class Main
 
         contextProto.close(); System.out.println("-------------------------------------------");
 
-        //
+        // задачи 7,8
         ClassPathXmlApplicationContext contextSingle =
                 new ClassPathXmlApplicationContext("applicationContextSingle.xml");
 
@@ -53,8 +60,5 @@ public class Main
         car6.go();
 
         contextSingle.close();
-
-
-
     }
 }
