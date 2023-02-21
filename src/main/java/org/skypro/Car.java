c2c63cd256fcb271c44a4ef293ebf6062f49fff4
garry <garry4978@gmail.com>
2023-02-21 04:20:09 +0300

    Урок 1. Spring. Введение

--- src/main/java/org/skypro/Car.java
+++ src/main/java/org/skypro/Car.java
@@ -1,9 +1,20 @@
 package org.skypro;
 
 public class Car {
+    public Car() {
+        System.out.println("Создан бин для Car");
+    }
 
     public void go(){
         System.out.println("Автомобиль едет \n");
+    }
+
+    public void init(){
+        System.out.println("Car.init \n");
+    }
+
+    public void destroy(){
+        System.out.println("Car.destroy \n");
     }
 
 }
--- src/main/java/org/skypro/Main.java
+++ src/main/java/org/skypro/Main.java
@@ -15,12 +15,45 @@ public class Main
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
         var  car = context.getBean("someCar", Car.class);
+        var  car2 = context.getBean("someCar", Car.class);
         car.go();
 
         var  person  = context.getBean("somePerson", Person.class);
         person.sitInCar();
 
-        context.close();
+        System.out.println(car==car2);
+        System.out.println(car + "  " + car2);
+
+        context.close(); System.out.println("-------------------------------------------");
+
+
+        ClassPathXmlApplicationContext contextProto =
+                new ClassPathXmlApplicationContext("applicationContextProto.xml");
+
+        var  car3 = contextProto.getBean("someCar", Car.class);
+        var  car4 = contextProto.getBean("someCar", Car.class);
+
+        System.out.println(car3==car4);
+        System.out.println(car3 + "  " + car4);
+
+
+        var  car5 = contextProto.getBean("someCar", Car.class);
+        car5.go();
+        System.out.println("вызов Car5.destroy");
+        car5.destroy();
+
+        contextProto.close(); System.out.println("-------------------------------------------");
+
+        //
+        ClassPathXmlApplicationContext contextSingle =
+                new ClassPathXmlApplicationContext("applicationContextSingle.xml");
+
+        System.out.println("Car6 ещё не создан");
+        var  car6 = contextSingle.getBean("someCar", Car.class);
+        car6.go();
+
+        contextSingle.close();
+
 
 
     }
